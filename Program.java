29e69a5fd26938030e77d7a8e913a362b5dc3152
Aditi <109041004+Aditih5573@users.noreply.github.com>
2024-08-28 12:09:09 -0700

    Update Program.java

--- Program.java
+++ Program.java
@@ -5,5 +5,24 @@
 //Paste console output below:
 /*
 
+import java.util.Scanner
+
+// Online Java Compiler
+
+// Use this editor to write, compile and run your Java code online
+
+class HelloWorld {
+    public static void main(String[] args) {
+    Scanner number = new Scanner(System.in);
+    System.out.println("Enter number 1: ");
+    int num = number.nextInt();
+    System.out.println("Enter number 2: ");
+    int num = number.nextInt();
+    
+       
+        
+    }
+}
+
 
 */
